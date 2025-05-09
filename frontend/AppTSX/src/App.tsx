f9afd26ab36fc1db57b3978466bdacf2879b04ad
Alejandro <alejandro.452001@gmail.com>
2025-05-09 14:29:00 -0300

    Se importo la react-router-dom, para crear un nav y poder moverse entre paginas

--- frontend/AppTSX/src/App.tsx
+++ frontend/AppTSX/src/App.tsx
@@ -1,51 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
-import { useEffect } from 'react'
-import getAllTrabajadores from './Apis/APITrabajadores'
-
-
+// src/App.tsx
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "@/pages/Home"
+import Products from "@/pages/Products"
+import { Navbar } from "@/components/ui/navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  useEffect(() => {
-    const apiUrl = `${import.meta.env.VITE_API_URL}/trabajadores/getAllTrabajadores`;
-    getAllTrabajadores(apiUrl)
-    .then((data) => {
-      console.log('Trabajadores:', data);
-    })
-    .catch((error) => {
-        console.error('Error fetching trabajadores:', error);
-      });
-  }, [])
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/AppTSX/src/components/ui/navbar.tsx
@@ -0,0 +1,11 @@
+// src/components/Navbar.tsx
+import { Link } from "react-router-dom"
+
+export function Navbar() {
+  return (
+    <nav className="flex gap-4 p-4 bg-gray-100">
+      <Link to="/">Home</Link>
+      <Link to="/products">About</Link>
+    </nav>
+  )
+}
