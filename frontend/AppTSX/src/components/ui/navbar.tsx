285e9940777c82a563bc97745746c9ff649f21bc
Alejandro <alejandro.452001@gmail.com>
2025-05-24 01:53:18 -0300

    Navbar /Home

--- frontend/AppTSX/src/components/ui/navbar.tsx
+++ frontend/AppTSX/src/components/ui/navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom"
 export function Navbar() {
   return (
     <nav className="flex gap-4 p-4 bg-gray-100">
-      <Link to="/">Home</Link>
+      <Link to="/Home">Home</Link>
       <Link to="/products">Products</Link>
       <Link to="/login">Login</Link>
       <Link to="/reset-password">Reset Password</Link>
