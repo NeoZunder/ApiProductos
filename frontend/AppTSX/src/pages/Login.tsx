4e2b4284a1be4d5e4fa9d6869fc853d559209a9b
Alejandro <alejandro.452001@gmail.com>
2025-05-24 02:12:30 -0300

    Actualizamos url para ir a reset-password

--- frontend/AppTSX/src/pages/Login.tsx
+++ frontend/AppTSX/src/pages/Login.tsx
@@ -17,6 +17,7 @@ import {
 } from "@/components/ui/tabs"
 
 import {functionLogin, functionRegister} from "@/Apis/AuthLogin"
+const FRONTEND_URL = import.meta.env.VITE_FRONTEND_URL || "http://localhost:5173";
 
 const buttonLogin = async () => {
     const loginMsg = document.querySelector(".login-msg") as HTMLDivElement;
@@ -67,7 +68,7 @@ export default function Login() {
                     <Label htmlFor="password">Password</Label>
                     <Input id="password" type="password"  defaultValue="" placeholder=""/>
                     </div>
-                    <a className="text-blue-700 underline font-bold text-xs" href="http://localhost:5173/reset-password">Forgot Your Password?</a>
+                    <a className="text-blue-700 underline font-bold text-xs" href={`${FRONTEND_URL}/reset-password`}>Forgot Your Password?</a>
                 </CardContent>
                 <CardFooter>
                     <Button
